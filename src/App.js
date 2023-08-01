0e504328e916f9284a7db247a6e12b97b6e94261
GoharMnoyan <mnoyangohar9@gmail.com>
2023-08-01 23:25:29 +0400

    abc

--- src/App.js
+++ src/App.js
@@ -2,11 +2,125 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div >
-      
+  
+  const names = [
+    {
+      name: "John",
+      surname: "Doe",
+      age: 47,
+      r: 172,
+      g: 214,
+      b: 199
+    },
+    {
+      name: "Jane",
+      surname: "Smith",
+      age: 28,
+      r: 203,
+      g: 185,
+      b: 220
+    },
+    {
+      name: "Alice",
+      surname: "Johnson",
+      age: 33,
+      r: 225,
+      g: 253,
+      b: 241
+    },
+    {
+      name: "Bob",
+      surname: "Brown",
+      age: 65,
+      r: 248,
+      g: 192,
+      b: 255
+    },
+    {
+      name: "Charles",
+      surname: "Davis",
+      age: 50,
+      r: 215,
+      g: 234,
+      b: 255
+    },
+    {
+      name: "Eve",
+      surname: "White",
+      age: 70,
+      r: 234,
+      g: 250,
+      b: 210
+    },
+    {
+      name: "Mallory",
+      surname: "Harris",
+      age: 38,
+      r: 255,
+      g: 244,
+      b: 172
+    },
+    {
+      name: "Sam",
+      surname: "Clark",
+      age: 80,
+      r: 190,
+      g: 200,
+      b: 253
+    },
+    {
+      name: "Trudy",
+      surname: "Thompson",
+      age: 15,
+      r: 233,
+      g: 222,
+      b: 171
+    },
+    {
+      name: "Oscar",
+      surname: "Anderson",
+      age: 95,
+      r: 199,
+      g: 204,
+      b: 251
+    }
+  ]
+  
+  
+  
+  
+  
+  
+  return(
+
+
+
+    <div style={{  display:"flex",margin:"80px",width:400}}>
+<div>
+
+{names.map(x=><div style={{border:"1px solid black",borderRadius:5,padding:20}}>{x.name}</div>)}
+
+</div>
+<div>
+{names.map(x=><div style={{border:"1px solid black",borderRadius:5,padding:20}}>{x.surname}</div>)}
+</div>
+<div style={{width:"47"}}>
+{names.map(x=><div style={{border:"1px solid black",borderRadius:5,padding:20}}>{x.age}</div>)}
+
+</div>
+
+
+
+
     </div>
-  );
+  )
+    
+  
+  
+  
+
+
+
 }
 
 export default App;
